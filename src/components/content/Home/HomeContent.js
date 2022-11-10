7844bff7946e1e3d11f1bf00e3ab68f8670cacda
JWSchmidt1 <jannickws@hotmail.com>
2022-11-10 23:19:15 +0100

    commit5

--- src/components/content/Home/HomeContent.js
+++ src/components/content/Home/HomeContent.js
@@ -1,11 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import gsap from "gsap";
+
 const HomeContent = () => {
+
+    let navigate = useNavigate(); 
+
+    const routeChange = () =>{ 
+      let path = `priser`; 
+      navigate(path);
+    }
+
+    const onlineBooking = () =>{ 
+        window.open('https://salonbook.one/?salon.naja#/', '_blank')
+    }
+
+    // window.onload = () => {
+
+    //     gsap.to(document.querySelector(".homeContentHeaderOne"), {
+    //         duration: 2,
+    //         borderBottom: "1px solid black",
+    //         transformOrigin: "center",
+    //         ease: "elastic.out",
+    //     })
+
+    // }
+
     return(
         <>
             <div className="homeContentInfoCon">
                 <h2 className="homeContentHeaderOne">Salon Naja <span></span></h2>
                 <h2  className="homeContentHeaderTwo"><span></span> 8660 Skanderborg</h2>
-                <button className="homeContentBook">Book tid</button>
-                <button className="homeContentKontakt">Kontakt</button>
+                <button className="homeContentBook" onClick={() => onlineBooking()}>Book tid</button>
+                <button className="homeContentKontakt" onClick={routeChange}>Priser</button>
             </div>
             <div className="homeContentImgConPLUS">
                 <div className="homeContentImgCon">

b13729c50935509376478b2e817b65faf293b994
JWSchmidt1 <jannickws@hotmail.com>
2022-11-10 13:39:59 +0100

    commit4

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -6,10 +6,41 @@ const Navigation = () => {
         <div className="navigation">
             <nav>
                 <ul>
-                    <li><Link to = "/"><span>Hjem</span></Link></li>
+                    {/* <li><Link to = "/"><span>Hjem</span></Link></li>
                     <li><a href="https://salonbook.one/?salon.naja#/" target="_blank"><span>Book tid</span></a></li>
                     <li><Link to = "/priser"><span>Priser</span></Link></li>
-                    <li><Link to = "/kontakt-os"><span>Kontakt</span></Link></li>
+                    <li><Link to = "/kontakt-os"><span>Kontakt</span></Link></li> */}
+
+                    <li>
+                        <NavLink
+                        to=""
+                        className={({ isActive }) =>
+                        isActive ? "activeClassName" : undefined
+                        }>
+                            <span>Hjem</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <a href="https://salonbook.one/?salon.naja#/" target="_blank"><span>Book tid</span></a>
+                    </li>
+                    <li>
+                        <NavLink
+                        to="priser"
+                        className={({ isActive }) =>
+                        isActive ? "activeClassName" : undefined
+                        }>
+                            <span>Priser</span>
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                        to="kontakt"
+                        className={({ isActive }) =>
+                        isActive ? "activeClassName" : undefined
+                        }>
+                            <span>KOntakt</span>
+                        </NavLink>
+                    </li>
                 </ul>
             </nav>
         </div>
